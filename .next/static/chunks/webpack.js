fd82e97834852942c64a4e4f648fe64e4ca8385b
enpleinair23 <enpleinair@mail.com>
2024-03-10 21:19:50 +0000

    yyy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bada05a53a78e266"; }
+/******/ 		__webpack_require__.h = function() { return "cbbf619378fcda2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -316,7 +316,7 @@ const Portfolio = () => {
                       />
                       <div
                         className="main absolute inset-0 bg-no-repeat bg-cover bg-center"
-                        data-img-url="assets/img/portfolio/invest.jpg"
+                        data-img-url="assets/img/portfolio/northatlantic.jpg"
                       />
                       <div className="details">
                         <h3 className="text-[16px] mb-[2px] font-semibold">
